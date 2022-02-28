bd05647b4b141ded12a88bcf4a1cd4c0845b46d0
luorongxi <492699405@qq.com>
2022-02-28 11:45:08 +0800

    v3.2.2版本串口通讯接收指令数据拆包处理代码优化，网口通讯增加支持三色报警灯控制指令。

--- iot-modbus-test/src/main/java/com/takeoff/iot/modbus/test/controller/TestController.java
+++ iot-modbus-test/src/main/java/com/takeoff/iot/modbus/test/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.takeoff.iot.modbus.test.controller;
 
 import com.takeoff.iot.modbus.common.data.MiiData;
+import com.takeoff.iot.modbus.common.entity.AlarmLampData;
 import com.takeoff.iot.modbus.common.entity.LcdData;
 import com.takeoff.iot.modbus.serialport.service.SerialportSendService;
 import com.takeoff.iot.modbus.test.config.IotModbusServerConfig;
@@ -105,7 +106,7 @@ public class TestController {
     }
 
     /**
-     * 发送背光灯指令
+     * 批量发送lCD控制指令
      * @param lcdDataList
      * @return
      */
@@ -117,6 +118,20 @@ public class TestController {
             iotModbusServerConfig.getMiiServer().sender().lcdBatch(lcdDataList);
         }
         return R.ok();
+    }
+
+    /**
+     * 发送三色报警灯指令
+     * @param alarmLampData
+     * @return
+     */
+    @RequestMapping("/alarmlamp")
+    public void alarmLamp(@RequestBody AlarmLampData alarmLampData) {
+        if(iotModbusSerialportProperties.getOpen()){
+
+        }else{
+            iotModbusServerConfig.getMiiServer().sender().alarmLamp(alarmLampData);
+        }
     }
 
 }
